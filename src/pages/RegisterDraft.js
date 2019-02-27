e95654f8ef06c6ac1332756d07064540a98ed230
sabit990928 <sabit990928@gmail.com>
2019-02-27 13:59:57 +0600

    feat(RegisterDraft): gty

---
+++ src/pages/RegisterDraft.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Form, Input, Button, Icon, Row, Col } from "antd";
+
+const FormItem = Form.Item;
+
+class RegisterDraft extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.props.registerUser(values, this.props.history);
+      }
+    });
+  };
+
+  render() {
+    // const { getFieldDecorator } = this.props.form;
+
+    return (
+      <Row type="flex" justify="center">
+        <Col span={6}>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <FormItem>
+              {/* {getFieldDecorator("username", {
+                rules: [
+                  { required: true, message: "Please input your username!" }
+                ]
+              })( */}
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="Username"
+                />
+              {/* )} */}
+            </FormItem>
+            <FormItem>
+              {/* {getFieldDecorator("password", {
+                rules: [
+                  { required: true, message: "Please input your Password!" }
+                ]
+              })( */}
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  type="password"
+                  placeholder="Password"
+                />
+              {/* )} */}
+            </FormItem>
+            <FormItem>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Register
+              </Button>
+              Or <a href="/login">login now!</a>
+            </FormItem>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default RegisterDraft;
