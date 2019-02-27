71fb59832095973c7b6f6ecc87ada45e3cbc8e90
sabit990928 <sabit990928@gmail.com>
2019-02-27 13:58:28 +0600

    feat(Login): add functionality to send data

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,69 +1,74 @@
 import React, { Component } from "react";
-import { Row, Col, Form, Input, Button, Icon } from "antd";
+import axios from 'axios';
+import styled from 'styled-components';
 
-const FormItem = Form.Item;
-
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 class Login extends Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        this.props.loginUser(values, this.props.history);
-      }
-    });
+
+  state = {
+    email: '',
+    password: '',
+    result: {}
   };
 
+  // componentDidMount() {
+  //   axios.get('http://10.8.130.113/tamaqtan/test.php')
+  //     .then(res => {
+  //   console.log("persons here: ", res)
+  //     })
+  //     .catch(res => {
+  //       console.log(res.data)
+  //     })
+  // }
+
+  handleEmailChange = (event) =>{
+    this.setState({ email: event.target.value })
+  }
+
+  handlePasswordChange = (event) =>{
+    this.setState({ password: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+
+    const user = {
+      login: email,
+      password: password
+    }
+
+    axios.post(`http://10.8.130.113/tamaqtan/test.php`, { user })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(res =>{
+        console.log(res)
+      }
+      )
+  }
+
   render() {
-    // const { getFieldDecorator } = this.props.form;
 
     return (
-      <div style={{ height: "calc(100vh - 86px)", width: '90%', margin: 'auto', marginTop: 20, backgroundColor: 'transparent', marginBottom: 20 }}>
-        <Row type="flex" justify="center">
-          <Col span={6}>
-            <Form onSubmit={this.handleSubmit} className="login-form">
-              <FormItem>
-                {/* {getFieldDecorator("username", {
-                rules: [
-                  { required: true, message: "Please input your username!" }
-                ]
-              })( */}
-                <Input
-                  prefix={
-                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="Username"
-                />
-              {/* )} */}
-              </FormItem>
-              <FormItem>
-                {/* {getFieldDecorator("password", {
-                rules: [
-                  { required: true, message: "Please input your Password!" }
-                ]
-              })( */}
-                <Input
-                  prefix={
-                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  type="password"
-                  placeholder="Password"
-                />
-              {/* )} */}
-              </FormItem>
-              <FormItem>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="login-form-button"
-                >
-                Login
-                </Button>
-              Or <a href="/register">register now!</a>
-              </FormItem>
-            </Form>
-          </Col>
-        </Row>
-      </div>
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <label>
+            Email: 
+            <input type='text' name='email' onChange={this.handleEmailChange} />
+          </label>
+          <label>
+            Password: 
+            <input type='password' name='password' onChange={this.handlePasswordChange} />
+          </label>
+          <button type='submit'>Login</button>
+        </Form>
+          Or <a href="/register">register</a> now
+      </div>  
     );
   }
 }
