2af4b7f552a82b5da881d25f33e2585c75a6a932
Bekw <bekwitmail@gmail.com>
2019-03-05 14:12:19 +0600

    add back

---
+++ back/api/objects/user.php
@@ -0,0 +1,90 @@
+<?php
+// класс для нашего юзера
+class User{
+ 
+    
+    private $conn;
+    private $table_name = "users";
+ 
+    
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $password;
+ 
+    
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ // функция для создания юзера
+function create(){
+ 
+    
+    $query = "INSERT INTO " . $this->table_name . "
+            SET
+                firstname = :firstname,
+                lastname = :lastname,
+                email = :email,
+                password = :password";
+ 
+    $stmt = $this->conn->prepare($query);
+ 
+    $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+    $this->lastname=htmlspecialchars(strip_tags($this->lastname));
+    $this->email=htmlspecialchars(strip_tags($this->email));
+    $this->password=htmlspecialchars(strip_tags($this->password));
+ 
+    $stmt->bindParam(':firstname', $this->firstname);
+    $stmt->bindParam(':lastname', $this->lastname);
+    $stmt->bindParam(':email', $this->email);
+    // Криптография 
+    $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password_hash);
+ 
+    
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+ 
+
+//Проверка есть ли такой юзер
+function emailExists(){
+ 
+
+    $query = "SELECT id, firstname, lastname, password
+            FROM " . $this->table_name . "
+            WHERE email = ?
+            LIMIT 0,1";
+ 
+    $stmt = $this->conn->prepare( $query );
+ 
+    $this->email=htmlspecialchars(strip_tags($this->email));
+ 
+    $stmt->bindParam(1, $this->email);
+ 
+    $stmt->execute();
+ 
+    $num = $stmt->rowCount();
+ 
+    if($num>0){
+ 
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        $this->id = $row['id'];
+        $this->firstname = $row['firstname'];
+        $this->lastname = $row['lastname'];
+        $this->password = $row['password'];
+ 
+        return true;
+    }
+ 
+    return false;
+}
+ 
+// Нужно дописать код 
+// Добавьте сюда функцию апдейт
+}
