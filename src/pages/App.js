c63dd9b262d6287c486406e9e289d377ba13964f
sabit990928 <sabit990928@gmail.com>
2019-03-27 12:21:33 +0600

    fix(App): change link to initial page

--- src/pages/App.js
+++ src/pages/App.js
@@ -21,7 +21,7 @@ class App extends Component{
           {/* <Route exact path="/welcome" component={Welcome} /> */}
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/profile" component={Profile} />
         </Layout>
       </BrowserRouter>
