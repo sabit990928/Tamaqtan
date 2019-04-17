c29b5c2e33d7957f7a9f580473009a4670b73be7
Sabit <sabit990928@gmail.com>
2019-04-17 12:55:58 +0600

    feat(App): fix routes

--- src/pages/App.js
+++ src/pages/App.js
@@ -17,11 +17,12 @@ class App extends Component{
       <div className="App">
       <BrowserRouter>
         <Layout>
+          <HeaderExample />
           <Route exact path="/login" component={Login} />
-          {/* <Route exact path="/register" component={Register} />
+          <Route exact path="/register" component={Register} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/" component={Home} />
-          <Route exact path="/profile" component={Profile} />         */}
+          <Route exact path="/profile" component={Profile} />        
         </Layout>
       </BrowserRouter>
     </div>
