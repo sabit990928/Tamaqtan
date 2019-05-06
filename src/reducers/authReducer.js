b1e96043b0ad9bdaae03315bf57baa224950684d
Sabit <sabit990928@gmail.com>
2019-05-06 13:25:33 +0600

    feat(authReducer): add logout

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_USER } from '../actions/types';
+import { FETCH_USER, LOGOUT_USER } from '../actions/types';
 
 const INITIAL_STATE = null;
 
@@ -6,6 +6,8 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_USER: 
       return action.payload || null;
+    case LOGOUT_USER:
+      return null;
     default:
       return state;
   }
