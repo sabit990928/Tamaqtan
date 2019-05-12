ee66c0fa19cd8dc7d3b4b3bd520f7a85cda35e44
NurbolIT <tolegenov.n.2705@gmail.com>
2019-05-12 16:29:56 +0600

    Remove button and Add componentDidMount()

--- src/components/RandomFood.js
+++ src/components/RandomFood.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import  Tamaq  from "./Tamaq";
 import { Card, Col, Row, Button } from 'antd';
 
 class RandomFood extends Component {
+  componentDidMount(){
+    this.fetchRandomFood();
+  }
   state = {
     data: []
   }
@@ -18,21 +21,12 @@ class RandomFood extends Component {
       render() {
         const { data } = this.state;
         return (
+          
           <div>
-            <Button onClick={this.fetchRandomFood}>Найти случайное блюдо</Button>
-            { data.length > 0 && <div style={{ background: '#ECECEC'}} className="card">
-              <h1>Ваше блюдо</h1> 
-              <Row gutter={16}>
-      
-                <Col span={8}>
-                  <Card title={data[0].name} bordered={false} className="cards"><img src={data[0].img_address} className="img5"/>
-
-                  <br/>{data[0].type_name}<br/>{data[0].user_type_name}<br/>{data[0].time_name}<br/>
-                  <Button type="primary" className="button1">Показать</Button></Card>
-                </Col>
             
-              </Row>
-            </div>
+            {/* <Button onClick={this.fetchRandomFood}>Найти случайное блюдо</Button> */}
+            
+            { data.length > 0 && <Tamaq tamaq={data} />
             }
           </div>
         );
