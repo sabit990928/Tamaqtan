c8955d39f6d1882eed00ff26d272d6190e7f244d
Sabit <sabit990928@gmail.com>
2019-05-13 12:07:53 +0600

    feat(Menus): make request by food type

--- src/pages/Menus.js
+++ src/pages/Menus.js
@@ -15,15 +15,44 @@ class Menus extends Component{
   }
   
   componentDidMount(){
-    axios.get(`http://localhost/back/api/getMenuSport.php`)
-      .then(res => {
-        const data = res.data.records;
-        this.setState({ data });
-      }).catch(res => console.log("Err: ", res))
+
+    switch(this.props.location.state.type){
+      case "sport": 
+        return axios.get(`http://localhost/back/api/getMenuSport.php`)
+          .then(res => {
+            const data = res.data.records;
+            this.setState({ data });
+          }).catch(res => console.log("Err: ", res));
+      case "usual":
+        return axios.get(`http://localhost/back/api/getMenuUsuall.php`)
+        .then(res => {
+          const data = res.data.records;
+          this.setState({ data });
+        }).catch(res => console.log("Err: ", res));
+      case "ill":
+        return axios.get(`http://localhost/back/api/getMenuIll.php`)
+        .then(res => {
+          const data = res.data.records;
+          this.setState({ data });
+        }).catch(res => console.log("Err: ", res));
+      case "berem":
+        return axios.get(`http://localhost/back/api/getMenuBerem.php`)
+        .then(res => {
+          const data = res.data.records;
+          this.setState({ data });
+        }).catch(res => console.log("Err: ", res));
+      case "diet":
+        return axios.get(`http://localhost/back/api/getMenuDiet.php`)
+        .then(res => {
+          const data = res.data.records;
+          this.setState({ data });
+        }).catch(res => console.log("Err: ", res));
+      
+    }
 }
       render() {
         const { data } = this.state;
-
+        console.log(this.props, ' props')
         return (
           <div>
             <h2>Ваше меню</h2>
