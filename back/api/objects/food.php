9b264ff4c7a3496d54a96ff2d7c1ec3d5fddf669
Bekw <bekwitmail@gmail.com>
2019-05-13 12:26:14 +0600

    Add function getById

--- back/api/objects/food.php
+++ back/api/objects/food.php
@@ -163,7 +163,7 @@
 // вывод блюда по ID
         function readOne(){
 
-            $query = "SELECT f.id, f.name, f.recept, f.img_address,  t.name as time_name, ty.name as type_name, u.name as user_type_name, f.time_id, f.type_id, f.user_type_id   
+            $query = "SELECT f.id as id, f.name as name, f.recept as recept, f.img_address as img_address,  t.name as time_name, ty.name as type_name, u.name as user_type_name, f.time_id as time_id, f.type_id as type_id, f.user_type_id as user_type_id  
                       FROM " . $this->table_name . "  f
                       LEFT JOIN time t ON f.time_id = t.id
                       LEFT JOIN type ty ON f.type_id = ty.id
@@ -171,25 +171,45 @@
                       WHERE
                           f.id = ?
                       LIMIT
-                          0.1";
+                          0,1";
 
             $stmt = $this->conn->prepare($query);
 
             $stmt->bindParam(1, $this->id);
-
+            
             $stmt->execute();
 
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+           // $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            // $this->name = $row['name'];
+            // $this->recept = $row['recept'];
+            // $this->img_address = $row['img_address'];
+            // $this->time_name = $row['time_name'];
+            // $this->time_id = $row['time_id'];
+            // $this->type_name = $row['type_name'];
+            // $this->type_id = $row['type_id'];
+            // $this->user_type_name = $row['user_type_name'];
+            // $this->user_type_id = $row['user_type_id'];
+            return $stmt;
+        }
+        // для случайной еды
+        function get_random(){
 
-            $this->name = $row['name'];
-            $this->recept = $row['recept'];
-            $this->img_address = $row['img_address'];
-            $this->time_name = $row['time_name'];
-            $this->time_id = $row['time_id'];
-            $this->type_name = $row['type_name'];
-            $this->type_id = $row['type_id'];
-            $this->user_type_name = $row['user_type_name'];
-            $this->user_type_id = $row['user_type_id'];
+            $query = "SELECT f.id, f.name, f.recept, f.img_address,  t.name as time_name, ty.name as type_name, u.name as user_type_name, f.time_id, f.type_id, f.user_type_id   
+                      FROM " . $this->table_name . " f
+                      LEFT JOIN time t ON f.time_id = t.id
+                      LEFT JOIN type ty ON f.type_id = ty.id
+                      LEFT JOIN user_type u ON f.user_type_id = u.id
+                      WHERE u.id = 1
+                      ORDER BY RAND()
+                      LIMIT 1";
+                
+            $stmt = $this->conn->prepare($query);
+        
+            $stmt->execute();
+ 
+             return $stmt;
+
         }
     
 // nado dobit'
@@ -213,25 +233,7 @@
 
             return $stmt;
         }
-// для случайной еды
-        function get_random(){
 
-            $query = "SELECT f.id, f.name, f.recept, f.img_address,  t.name as time_name, ty.name as type_name, u.name as user_type_name, f.time_id, f.type_id, f.user_type_id   
-                      FROM " . $this->table_name . " f
-                      LEFT JOIN time t ON f.time_id = t.id
-                      LEFT JOIN type ty ON f.type_id = ty.id
-                      LEFT JOIN user_type u ON f.user_type_id = u.id
-                      WHERE u.id = 1
-                      ORDER BY RAND()
-                      LIMIT 1";
-                
-            $stmt = $this->conn->prepare($query);
-        
-            $stmt->execute();
- 
-             return $stmt;
-
-        }
 
     }
 ?>
--- back/api/readOne.php
+++ back/api/readOne.php
@@ -1,43 +1,51 @@
 <?php
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Credentials: true");
-header('Content-Type: application/json');
-
-include_once 'config/database.php';
-include_once 'objects/food.php';
-
-$database = new Database();
-$db = $database->getConnection();
-
-$food = new Food($db);
-
-$food->id = isset($_GET['id']) ? $_GET['id'] : die();
-$food->readOne();
-
-if($food->name!=null){
-
-    $food_arr = array(
-
-        "id" => $food->$id,
-        "name" => $food->$name,
-        "recept" => $food->$recept,
-        "img_address" => $food->$img_address,
-        "time_name" => $food->$time_name,
-        "type_name" => $food->$type_name,
-        "user_type_name" => $food->$user_type_name
-
-    );
-    http_response_code(200);
-    echo json_encode($food_arr);
-
-}else{
-    http_response_code(404);
-    echo json_encode(array("message" => "Food does not exist."));
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+     
+    include_once 'config/database.php';
+    include_once 'objects/food.php';
     
-}
+    $database = new Database();
+    $db = $database->getConnection();
+    
+    $food = new Food($db);
 
-
+    $food->id = isset($_GET['id']) ? $_GET['id'] : die();
+    $stmt = $food->readOne();
+   // $stmt = $food->get_random();
+    $num = $stmt->rowCount();
+    
+    if($num>0){
+ 
+        $food_arr=array();
+        $food_arr["records"]=array();
+     
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+     
+            $food_item=array(
+                "id" => $id,
+                "name" => $name,
+                "recept" => html_entity_decode($recept),
+                "img_address" => html_entity_decode($img_address),
+                "time_name" => $time_name,
+                "type_name" => $type_name,
+                "user_type_name" => $user_type_name
+            );
+     
+            array_push($food_arr["records"], $food_item);
+        }
+     
+        http_response_code(200);
+     
+        echo json_encode($food_arr);
+    }
+    else{
+ 
+        http_response_code(404);
+     
+        echo json_encode(
+            array("message" => "No food found.")
+        );
+    }
 ?>
